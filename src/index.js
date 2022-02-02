1d7f142d6742b370c07b042e34d8afce1a7f3cf3
Lazar <lazard1611@gmail.com>
2022-02-02 13:32:14 +0200

    fetch (To do list app) basic structyre

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('Hello')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const AppHeader = () => {
+    return (
+        <h1>Hello</h1>
+    )
+}
+
+const SearhPanel = () => {
+
+    const searchText = 'Type here to search';
+    const searchStyle = {
+        fontSize: '20px'
+    };
+    return (
+        <input
+            style={searchStyle}
+            placeholder = {searchText} />
+    )
+}
+
+const TodoList = () => {
+    const item = ['Learn React', 'Build Awesome App'];
+
+    return (
+        <ul>
+            <li>{item[0]}</li>
+            <li>{item[1]}</li>
+        </ul>
+    )
+}
+
+const App = () => {
+
+    return (
+        <div>
+            <AppHeader />
+            <SearhPanel />
+            <TodoList />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
